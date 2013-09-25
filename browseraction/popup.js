0bde98e401909ef53285dd00e74c629ccfa9cce4
kevmo <k.james.moore@gmail.com>
2013-09-25 09:16:00 -0700

    working on view

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -1,5 +1,9 @@
 //VIEWS - dynamically create 
 
+var post, link;
+
+post = 
+
 //listen for user choice of either post or link, then generate appropriate form
 
 // check localstorage and display what's in hopper
