22fbe79f8a55cd2b92d1e637b9a6c007c5c5bc82
kevmo <k.james.moore@gmail.com>
2013-09-26 09:54:40 -0700

    writing to local storage

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -3,4 +3,24 @@
 
 //functions 
 
-// check localstorage and display what's in hopper
+
+
+function store(formObj){
+  console.log("formObj", formObj);
+  var message = JSON.stringify(formObj);
+  window.localStorage.setItem("testing", message);
+  console.log("JSON? --->", message);
+}
+
+function checkSubmitting(){
+  console.log("The onsubmit event is running");
+}
+
+function checkOnClick(){
+  console.log("onclick is working");
+}
+
+$( "#plz" ).click(function( event ) {
+  console.log( $( this ).serializeArray() );
+  event.preventDefault();
+});
