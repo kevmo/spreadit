7db0c88fb55d9e8f43ae368518ec06e113a235ed
kevmo <k.james.moore@gmail.com>
2013-09-26 11:46:17 -0700

    successfully serializing form input --> JSON object

--- browseraction/popup.js
+++ browseraction/popup.js
@@ -1,16 +1,5 @@
-//VIEWS - dynamically create 
 
-
-//functions 
-
-
-
-function store(formObj){
-  console.log("formObj", formObj);
-  var message = JSON.stringify(formObj);
-  window.localStorage.setItem("testing", message);
-  console.log("JSON? --->", message);
-}
+// BOOTLEG TESTING 
 
 function checkSubmitting(){
   console.log("The onsubmit event is running");
@@ -20,7 +9,76 @@ function checkOnClick(){
   console.log("onclick is working");
 }
 
+// Serializing to JSON helper function (jquery)
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+// Create and display that JSON object 
+
+$(function() {
+    $('form').submit(function() {
+        $('#result').text(JSON.stringify($('form').serializeObject()));
+        // prevent submission
+        return false;
+    });
+});
+
+
+// Send JSON Object to local Storage 
+
+
+
+
+
+
+
+
+
+
 $( "#plz" ).click(function( event ) {
   console.log( $( this ).serializeArray() );
   event.preventDefault();
 });
+
+function store(formObj){
+  console.log("formObj", formObj);
+  var message = JSON.stringify(formObj);
+  window.localStorage.setItem("testing", message);
+  console.log("JSON? --->", message);
+}
+
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+function toLS(){
+}
